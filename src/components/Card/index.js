a27653647c38499ae932046fc9a67e11999a6000
Andriw Fonseca <filipefonsequinha@gmail.com>
2022-12-19 20:16:32 -0300

    criado função para gerar imagens conforme o que digitou

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,19 +1,21 @@
-const Card = () => {
-  return (
-    <div class="card" style={{ width: "25%" }}>
-      <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPyTwys7LrLAWu9VqXGl2EDwOP-iQr1h0WuxUG_MJRttAwQZ2i3dBHmJpzg5L9rLHYPFU&usqp=CAU"
-        class="card-img-top"
-        alt="Tênis Nike"
-      />
-      <div class="card-body">
-        <h5 class="card-title">Tênis Nike</h5>
-        <a href="#" class="btn btn-danger">
-          Remover da lista
-        </a>
-      </div>
-    </div>
-  );
+const Card = ({ item }) => {
+    const keywords = item?.title?.split(" ").join(",");
+
+    return (
+        <div class="card" style={{ width: "25%", padding: 0 }}>
+            <img
+                src={`https://loremflickr.com/320/240/${keywords}`}
+                class="card-img-top"
+                alt="Tênis Nike"
+            />
+            <div class="card-body">
+                <h5 class="card-title">{item?.title}</h5>
+                <a href="#" class="btn btn-danger">
+                    Remover da lista
+                </a>
+            </div>
+        </div>
+    );
 };
 
 export default Card;
--- src/pages/List/index.js
+++ src/pages/List/index.js
@@ -1,33 +1,54 @@
+import { useState } from "react";
 import Card from "../../components/Card";
+import uuid from "react-uuid";
 
 const List = () => {
-  return (
-    <div className="container">
-      <div className="col-6 offset-3 mt-5">
-        <h1 className="mb-2">Lista de Desejos</h1>
-        <div class="input-group mb-3">
-          <input
-            type="text"
-            class="form-control"
-            placeholder="Adicione um desejo"
-          />
-          <button class="btn btn-outline-success" type="button">
-            Adicionar Desejo
-          </button>
+    const [wishItem, setWishItem] = useState("");
+    const [wishList, setWishList] = useState([
+        {
+            id: uuid(),
+            title: "Tênis Nike",
+        },
+    ]);
+
+    const addWishItem = () => {
+        const newWishItem = {
+            id: uuid(),
+            title: wishItem,
+        };
+
+        setWishList([...wishList, newWishItem]);
+    };
+
+    return (
+        <div className="container">
+            <div className="col-6 offset-3 mt-5 text-center">
+                <h1 className="mb-2">Lista de Desejos</h1>
+                <div className="input-group mb-3">
+                    <input
+                        type="text"
+                        class="form-control"
+                        placeholder="Adicione um desejo"
+                        onChange={(e) => {
+                            setWishItem(e.target.value);
+                        }}
+                    />
+                    <button
+                        className="btn btn-outline-success"
+                        type="button"
+                        onClick={addWishItem}
+                    >
+                        Adicionar Desejo
+                    </button>
+                </div>
+            </div>
+            <div className="row mt-5">
+                {wishList?.map((item) => (
+                    <Card item={item} />
+                ))}
+            </div>
         </div>
-      </div>
-      <div className="row">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default List;
